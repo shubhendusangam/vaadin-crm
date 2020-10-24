daf8d045390f114790d4744b8dfb3d2fecc0da96
Shubhendu <shubhendu.sangam@riversand.com>
2020-10-24 12:50:47 +0530

    starter project setup

---
+++ src/main/java/com/vaadin/tutorials/ui/MainView.java
@@ -0,0 +1,11 @@
+package com.vaadin.tutorials.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+    }
+}
