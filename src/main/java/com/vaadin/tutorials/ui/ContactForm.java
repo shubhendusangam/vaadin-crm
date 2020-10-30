9516b49e4537b85a0a8ff47b1eaa905ee642bb21
Shubhendu <shubhendu.sangam@riversand.com>
2020-10-30 20:29:09 +0530

    Passing data and events between Vaadin components

--- src/main/java/com/vaadin/tutorials/ui/ContactForm.java
+++ src/main/java/com/vaadin/tutorials/ui/ContactForm.java
@@ -12,7 +12,6 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.shared.Registration;
--- src/main/java/com/vaadin/tutorials/ui/MainView.java
+++ src/main/java/com/vaadin/tutorials/ui/MainView.java
@@ -1,8 +1,10 @@
 package com.vaadin.tutorials.ui;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -27,15 +29,20 @@ public class MainView extends VerticalLayout {
         addClassName("list-view");
         setSizeFull();
         configureGrid();
-        configureFilter();
+        toolBar();
 
         contactForm = new ContactForm(companyService.findAll());
+        contactForm.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        contactForm.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        contactForm.addListener(ContactForm.CloseEvent.class, evt -> closeEditors());
+
         Div content = new Div(contactGrid, contactForm);
         content.addClassName("content");
         content.setSizeFull();
 
-        add(filterText, content);
+        add(toolBar(), content);
         updateList();
+        closeEditors();
     }
 
     private void configureGrid() {
@@ -48,16 +55,57 @@ public class MainView extends VerticalLayout {
             return company == null ? "-" : company.getName();
         }).setHeader("Company");
         contactGrid.getColumns().forEach(contactColumn -> contactColumn.setAutoWidth(true));
+        contactGrid.asSingleSelect().addValueChangeListener(event -> editContact(event.getValue()));
     }
 
-    private void configureFilter() {
+    private HorizontalLayout toolBar() {
         filterText.setPlaceholder("Filter by name ..");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
         filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add Contact");
+        addContactButton.addClickListener(click -> addContact());
+
+        HorizontalLayout toolBar = new HorizontalLayout(filterText, addContactButton);
+        toolBar.addClassName("toolbar");
+        return toolBar;
     }
 
     private void updateList() {
         contactGrid.setItems(contactService.findAll(filterText.getValue()));
+    }
+
+    private void addContact() {
+        contactGrid.asSingleSelect().clear();
+        editContact(new Contact());
+    }
+
+    private void editContact(Contact contact) {
+        if (contact == null){
+            closeEditors();
+        } else {
+            contactForm.setContact(contact);
+            contactForm.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent deleteEvent) {
+        contactService.delete(deleteEvent.getContact());
+        updateList();
+        closeEditors();
+    }
+
+    private void saveContact(ContactForm.SaveEvent saveEvent) {
+        contactService.save(saveEvent.getContact());
+        updateList();
+        closeEditors();
+    }
+
+    private void closeEditors() {
+        contactForm.setContact(null);
+        contactForm.setVisible(false);
+        removeClassName("editing");
     }
 }
